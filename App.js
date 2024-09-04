ee9dcd0b08faeddef789d978f4b0c09300d6a292
MustaphaAbk <132165784+MustaphaAbk@users.noreply.github.com>
2024-09-04 02:21:54 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+//https://www.figma.com/community/file/1403772079112349787
---
+++ README.md
@@ -0,0 +1,1 @@
+# base-of-mobile-app-RN
